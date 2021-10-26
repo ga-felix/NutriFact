e72e25b5ac208fe8e1cee0d5a490c5827de10dcd
ga-felix <gafelix435@gmail.com>
2021-10-25 21:46:21 -0300

    Initial commit

---
+++ src/main/java/com/nutrifact/NutriFactApplication.java
@@ -0,0 +1,13 @@
+package com.nutrifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriFactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriFactApplication.class, args);
+	}
+
+}
