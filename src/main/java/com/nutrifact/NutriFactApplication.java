16ebc0f1740bbbb83db13b4c336acd9e8a5d228a
TsuHub <tsuy_1988@hotmail.com>
2021-10-28 23:29:14 -0300

    refatoração do código

--- src/main/java/com/nutrifact/NutriFactApplication.java
+++ src/main/java/com/nutrifact/NutriFactApplication.java
@@ -1,9 +1,12 @@
-package com.nutrifact;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {""})
+//@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class NutriFactApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/nutrifact/NutriFactController.java
+++ src/main/java/com/nutrifact/NutriFactController.java
@@ -1,13 +1,24 @@
-package com.nutrifact;
+package com;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class NutriFactController
 {
-    @RequestMapping("/")
-    public String index() {
-        return "mainpage";
+    @GetMapping("/index")
+    public String ViewForm(Model model) {
+        model.addAttribute("CalculateIMC", new CalculateIMC());
+        return "index";
+    }
+
+    @PostMapping("/calculate")
+    public String calculateIMC(@ModelAttribute CalculateIMC calc, BindingResult result, Model model) {
+        model.addAttribute("calc", calc);
+        return "calculate";
     }
 }
