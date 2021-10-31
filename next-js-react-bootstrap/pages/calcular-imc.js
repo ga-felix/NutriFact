9dde533f2ef76dc8d3ac7f9b76cfdf180df264d9
TsuHub <tsuy_1988@hotmail.com>
2021-10-31 18:42:20 -0300

    página testes será deletado no próximo commit

--- next-js-react-bootstrap/pages/calcular-imc.js
+++ next-js-react-bootstrap/pages/calcular-imc.js
@@ -19,22 +19,22 @@ export default function Home () {
           <Col lg={8} md={8} sm={12} xs={12}>
             <Form>
               <Form.Group className='mb-3' controlId='formBasicEmail'>
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type='email' placeholder='Enter email' />
+                <Form.Label>Email</Form.Label>
+                <Form.Control type='email' placeholder='exemplo@exemplo.com' />
                 <Form.Text className='text-muted'>
-                  We'll never share your email with anyone else.
+                  Nós nunca compartilharemos seu email com ninguém.
                 </Form.Text>
               </Form.Group>
 
               <Form.Group className='mb-3' controlId='formBasicPassword'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control type='password' placeholder='Password' />
+                <Form.Label>Senha</Form.Label>
+                <Form.Control type='password' placeholder='********' />
               </Form.Group>
               <Form.Group className='mb-3' controlId='formBasicCheckbox'>
                 <Form.Check type='checkbox' label='Check me out' />
               </Form.Group>
               <Button variant='primary' type='submit'>
-                Submit
+                Enviar
               </Button>
             </Form>
           </Col>
