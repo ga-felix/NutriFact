15385063b32a4534acfdf809ad8799764fcef501
Vinicius Santana <vsviniciuslima@usp.br>
2021-11-08 19:35:40 -0300

    antes de upar a versao do bootstrap

--- next-js-react-bootstrap/pages/calcular-imc.js
+++ next-js-react-bootstrap/pages/calcular-imc.js
@@ -27,7 +27,12 @@ export default function Home () {
                   We'll never share your email with anyone else.
                 </Form.Text>
               </Form.Group>
-
+              <Form.Select aria-label='Default select example'>
+                <option>Open this select menu</option>
+                <option value='1'>One</option>
+                <option value='2'>Two</option>
+                <option value='3'>Three</option>
+              </Form.Select>
               <Form.Group className='mb-3' controlId='formBasicPassword'>
                 <Form.Label>Password</Form.Label>
                 <Form.Control type='password' placeholder='Password' />
--- next-js-react-bootstrap/pages/index.js
+++ next-js-react-bootstrap/pages/index.js
@@ -1,7 +1,11 @@
 import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Row, Col, Container } from 'react-bootstrap'
+
+import styles from './index.module.css'
+
+import Layout from '../components/layout/layout'
 
 export default function Home () {
   return (
@@ -13,26 +17,44 @@ export default function Home () {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <Form>
-        <Form.Group className='mb-3' controlId='formBasicEmail'>
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type='email' placeholder='Enter email' />
-          <Form.Text className='text-muted'>
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group className='mb-3' controlId='formBasicPassword'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control type='password' placeholder='Password' />
-        </Form.Group>
-        <Form.Group className='mb-3' controlId='formBasicCheckbox'>
-          <Form.Check type='checkbox' label='Check me out' />
-        </Form.Group>
-        <Button variant='primary' type='submit'>
-          Submit
-        </Button>
-      </Form>
+      <Layout>
+        <Container fluid>
+          <h1 id={styles.pageTitle}> Início </h1>
+          <Row>
+            <Col lg={2} md={2} sm={12} xs={12} />
+            <Col lg={8} md={8} sm={12} xs={12}>
+              <Form>
+                <Form.Group className='mb-3' controlId='formBasicEmail'>
+                  <Form.Label>Nome</Form.Label>
+                  <Form.Control type='text' placeholder='Insira seu nome' />
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='formBasicEmail'>
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type='email' placeholder='exemplo@exemplo.com' />
+                  <Form.Text className='text-muted'>
+                    Nós nunca compartilharemos seu email com ninguém.
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='formBasicPassword'>
+                  <Form.Label>Senha</Form.Label>
+                  <Form.Control type='password' placeholder='Senha' />
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='formBasicPassword'>
+                  <Form.Label>Confirme sua senha</Form.Label>
+                  <Form.Control type='password' placeholder='Senha' />
+                </Form.Group>
+                <Form.Group className='mb-3' controlId='formBasicCheckbox'>
+                  <Form.Check type='checkbox' label='Check me out' />
+                </Form.Group>
+                <Button variant='primary' type='submit'>
+                  Enviar
+                </Button>
+              </Form>
+            </Col>
+            <Col lg={2} md={2} sm={12} xs={12} />
+          </Row>
+        </Container>
+      </Layout>
     </>
   )
 }
